b0d69066fbcb824deab4bca153fc76933c153fe6
S-Kiev <vezequiel58@gmail.com>
2024-03-04 15:19:01 -0300

    reiniciando servicio

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -1,3 +1,11 @@
+const process = require('../shared/processMessage');
+const MetaAPIMedia = require('../services/Meta-API-Media');
+const whisper = require('../services/whisper-Service');
+const google = require('../services/GoogleAI-Service');
+
+const dotenv = require('dotenv');
+dotenv.config();
+
 const verifyToken = (req, res) =>{
     try {
         var accessToken = "bdbgdbgdbgrnyrtnytnfgnfg";
@@ -17,9 +25,7 @@ const verifyToken = (req, res) =>{
 }
 
 async function recivedMessage (req, res) {
-    res.send("Hola recibido");
-
-    /*
+    //console.log(req.body);
     try {
     var entry = (req.body['entry'])[0];
     var changes = (entry['changes'])[0];
@@ -45,7 +51,66 @@ async function recivedMessage (req, res) {
 
     res.send('EVENT_RECEIVED');
     }
-    */
+}
+
+async function getTextUser (messages) {
+
+    var text = '';
+    var typeMessage = messages['type'];
+
+    if (typeMessage == 'text') {
+        text = (messages['text'])['body'];
+    }
+    else if(typeMessage == 'audio'){
+
+        let idAudio = (messages['audio'])['id'];
+
+        let url = await MetaAPIMedia.getUrlMedia(idAudio);
+
+        let binaryAudio = await MetaAPIMedia.getBinaryMedia(url);
+
+        let transcription = await whisper.getTranscription(binaryAudio);
+
+        console.log(transcription);
+
+        text = transcription
+    }
+
+    else if (typeMessage == 'image' || typeMessage == 'video'){
+
+        let textUser;
+        let mediaType;
+
+        if (typeMessage == 'image') {
+            textUser = messages.image.caption ? messages.image.caption : null;
+            mediaType = 'jpeg';
+        }
+
+        if (typeMessage == 'video') {
+            textUser = messages.video.caption ? messages.video.caption : null;
+            mediaType = 'mp4';
+        }
+
+        
+        // 1. obtener el id del mensaje de imagen
+        let idImage = (messages['image'])['id'];
+        // 2. Pedir la url del audio en los servidores de Meta
+        let url = await MetaAPIMedia.getUrlMedia(idImage);
+        // 3. Obtener el binario de la imagen o video
+        let binaryMedia = await MetaAPIMedia.getBinaryMedia(url); 
+        // 4. procesar la imagen a formato png y enviarlo a google
+        let mediaDescription = await google.geminiImageService(binaryMedia, mediaType, textUser, typeMessage);
+
+        mediaDescription = mediaDescription.includes('factura') ? `Quiero facturar, aqui tienes la información \n\n ${mediaDescription}` : `Hola soy Gemini, y aqui te paso la descripción de una imagen que me paso el usuario: \n\n ${mediaDescription}`;
+
+        text = mediaDescription;
+
+    } 
+    else {
+        text = 'tipo de mensaje no valido';
+    }
+    //console.log(text);
+    return text;
 }
 
 
